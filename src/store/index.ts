1f76b04bf5690189f0552af06a400a15462f11b9
Olga Antushevich <antushevich@gmail.com>
2023-04-06 18:43:14 +0300

    feat: add middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,11 @@ import { githubApi } from './github/github.api';
 export const store = configureStore({
 	reducer: {
 		[githubApi.reducerPath]: githubApi.reducer,
-	}
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
+
+//! githubApi.reducer, githubApi.middleware - не создавались вручную, из создаёт сам RTKQuery
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
