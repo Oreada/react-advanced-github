cac8db1cb9c7b080822608de63af39dd5375dbce
Olga Antushevich <antushevich@gmail.com>
2023-04-11 18:47:24 +0300

    feat: add dropdown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import { useSearchUsersQuery } from "../store/github/github.api"
 
 export function HomePage() {
 	const [search, setSearch] = useState('');
+	const [dropdown, setDropdown] = useState(false);
 	const debounced = useDebounce(search);
 	const { data, isLoading, isError } = useSearchUsersQuery(debounced, {
 		//! это условие необходимо, чтобы не выполнялся запрос с пустым search, в результате которого выскакивает ошибка
@@ -18,7 +19,11 @@ export function HomePage() {
 
 	useEffect(() => {
 		console.log(debounced);
-	}, [debounced]);
+
+		if (debounced.length > 2 && data?.length) {
+			setDropdown(true);
+		};
+	}, [debounced, data]);
 
 	return (
 		<div>
@@ -33,9 +38,17 @@ export function HomePage() {
 						onChange={handleChange}
 					/>
 
-					<div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</div>
+					{dropdown && <ul className="list-none overflow-y-scroll absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+						{isLoading && <p className="text-center">Loading...</p>}
+						{data?.map((user) => (
+							<li
+								key={user.id}
+								className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+							>
+								{user.login}
+							</li>
+						))}
+					</ul>}
 				</div>
 			</div>
 		</div>
