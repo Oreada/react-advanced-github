2f8aec0fdc9598d908e0cf6cbee021918b5940a0
Olga Antushevich <antushevich@gmail.com>
2023-04-12 18:23:47 +0300

    add: create githubSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { githubApi } from './github/github.api';
+import { githubReducer } from './github/github.slice';
 
 export const store = configureStore({
 	reducer: {
 		[githubApi.reducerPath]: githubApi.reducer,
+		github: githubReducer,
 	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
