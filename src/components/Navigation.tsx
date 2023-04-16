b0eef495e5d35db92188e953a6b66e55d6c072c6
Olga Antushevich <antushevich@gmail.com>
2023-04-16 18:26:42 +0300

    feat: add styles for nav links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function Navigation() {
 	return (
 		<nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
 			<h3 className="font-bold">GitHub Search</h3>
 			<span>
-				<Link to='/' className="mr-2">Home</Link>
-				<Link to='/favorite'>Favorite</Link>
+				<NavLink to='/' end className={({ isActive }) => (isActive ? 'bg-white text-gray-500 mr-2 p-1' : 'mr-2 p-1')}>Home</NavLink>
+				<NavLink to='/favorite' className={({ isActive }) => (isActive ? 'bg-white text-gray-500 mr-2 p-1' : 'mr-2 p-1')}>Favorite</NavLink>
 			</span>
 		</nav>
 	);
