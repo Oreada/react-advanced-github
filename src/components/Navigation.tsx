e2f6b9e158f578d337436dc9b0bb4ca4b0064e40
Olga Antushevich <antushevich@gmail.com>
2023-04-18 12:18:41 +0300

    feat: add styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,8 +5,16 @@ export function Navigation() {
 		<nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
 			<h3 className="font-bold">GitHub Search</h3>
 			<span>
-				<NavLink to='/' end className={({ isActive }) => (isActive ? 'bg-white text-gray-500 mr-2 p-1' : 'mr-2 p-1')}>Home</NavLink>
-				<NavLink to='/favorite' className={({ isActive }) => (isActive ? 'bg-white text-gray-500 mr-2 p-1' : 'mr-2 p-1')}>Favorite</NavLink>
+				<NavLink to='/' end
+					className={({ isActive }) => (isActive ? 'bg-white text-gray-500 rounded mr-2 p-1' : 'mr-2 p-1')}
+				>
+					Home
+				</NavLink>
+				<NavLink to='/favorite'
+					className={({ isActive }) => (isActive ? 'bg-white text-gray-500 rounded mr-2 p-1' : 'mr-2 p-1')}
+				>
+					Favorite
+				</NavLink>
 			</span>
 		</nav>
 	);
