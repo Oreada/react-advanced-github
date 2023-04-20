fdea47d1fa37894e51a0b7e2145d2bf3c792647b
Olga Antushevich <antushevich@gmail.com>
2023-04-20 19:37:46 +0300

    feat: add paginationReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { githubApi } from './github/github.api';
 import { githubReducer } from './github/github.slice';
+import { paginationReducer } from './pagination.slice';
 
 export const store = configureStore({
 	reducer: {
 		[githubApi.reducerPath]: githubApi.reducer,
 		github: githubReducer,
+		pagination: paginationReducer,
 	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
